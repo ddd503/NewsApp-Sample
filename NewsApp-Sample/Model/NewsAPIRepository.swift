1292c5a29e99b5edc4c4b5aa618f75d0f7675e36
Dai Kawahara <skbsporta7@gmail.com>
2020-11-22 10:15:30 +0900

    testing combine

--- NewsApp-Sample/Model/NewsAPIRepository.swift
+++ NewsApp-Sample/Model/NewsAPIRepository.swift
@@ -6,13 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 protocol NewsAPIRepository {
     func request(_ completion: @escaping (Result<NewsAPIResponse, Error>) -> Void)
+    func request() -> AnyPublisher<NewsAPIResponse, Error>
 }
 
 struct NewsAPIRepositoryImpl: NewsAPIRepository {
     func request(_ completion: @escaping (Result<NewsAPIResponse, Error>) -> Void) {
         // TODO: NewsAPIから情報を取得する
+    }
+
+    func request() -> AnyPublisher<NewsAPIResponse, Error> {
+        // TODO: 通信を行い、URLSessionを通したPublishierを返す
+        return Future<NewsAPIResponse, Error> { promise in
+            let dummyResponse = NewsAPIResponse(items: [])
+            promise(.success(dummyResponse))
+        }
+        .eraseToAnyPublisher() // AnyPublisherに変換して返す
     }
 }
--- NewsApp-Sample/Model/NewsUseCase.swift
+++ NewsApp-Sample/Model/NewsUseCase.swift
@@ -6,12 +6,15 @@
 //
 
 import Foundation
+import Combine
 
 protocol NewsUseCase {
     func readNewsList(_ completion: @escaping (Result<[NewsArticle], Error>) -> Void)
+    func readNewsList() -> AnyPublisher<[NewsArticle], Error>
 }
 
 struct NewsUseCaseImpl: NewsUseCase {
+
     let newsAPIRepository: NewsAPIRepository
 
     init(newsAPIRepository: NewsAPIRepository) {
@@ -30,4 +33,24 @@ struct NewsUseCaseImpl: NewsUseCase {
             }
         }
     }
+
+    func readNewsList() -> AnyPublisher<[NewsArticle], Error> {
+        return Future<[NewsArticle], Error> { promise in
+            let _ = newsAPIRepository.request()
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        promise(.failure(error))
+                    case .finished:
+                        print("success")
+                    }
+                } receiveValue: { (newsAPIResponse) in
+                    promise(.success(newsAPIResponse.items.map {
+                        NewsArticle(category: $0.category, title: $0.title, content: $0.content, date: $0.date, img: $0.img)
+                    }))
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+
 }
